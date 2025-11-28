6a0bd69dc8236b3641564071557485551c14ad09
kharadepallavi38 <kharadepallavi38@gmail.com>
2025-11-28 04:19:27 -0800

    Update app.js

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use((req, res, next) => {
    res.locals.success = req.flash("success");
    res.locals.error = req.flash("error");
    res.locals.currUser = req.user;
-  next();
+  
 });
 
 app.use("/listings/:id/reviews", reviewsRouter);
